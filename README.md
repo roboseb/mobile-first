692e71b9750f0bd62660b76247f59e7e529965d9
roboseb <sebastienapril@rocketmail.com>
2022-05-22 17:19:23 -0600

    Add viewerbox to html. Add images folder. Add basic logic for scrolling images.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # mobile-first
 Project for the dynamic user interface interactions lesson of the Odin Project.
+
+---Features---
+
+
+
+---Devlog---
+
+May 25th 2022
+
+    Hey look at that, I finished the to-do list. And now for a short project that for some reason TOP didn't label as a project. Looks like fun anyway. One day project, that is the timeframe I'm putting on it.
+
+    Spent too much time drawing the pictures for this. Maybe should have just done what I did with the restaurant project. Regardless, will be done early tomorrow as I'm basically only adding the image viewer.
+
+
+---To-Do---
+
+DONE-Draw images for slider
+DONE-Add slider
+-Custom font
+-Prettier animations?
+-Drag through images on mobile, or click
+-Click through images on desktop, or use arrow keys.
+-dark mode if there's time
+-Make viewer endlessly scrollable
+-Have images ratchet in place, but scrollable
--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('test');
+//Drop down hamburger menu logic.
+const Menu = (() => {
+    const menuButton = document.getElementById('menubtn');
+    const menu = document.getElementById('menu');
+
+    //Get menu height so that it can animate properly, then hide it.
+    const menuHeight = `${menu.offsetHeight}px`;
+    menu.style.height = '0px';
+
+    let menuOpen = false;
+    
+
+    //Open menu if it is closed, otherwise open it.
+    menuButton.addEventListener('click', () => {
+        spin();
+
+        if (!menuOpen) {
+            menu.style.height = menuHeight;
+            menuOpen = true;
+        } else {
+            menu.style.height = '0px';
+            menuOpen = false;
+        }
+    });
+
+    let spinDegrees = 0;
+
+    //Animate menu button on click.
+    spin = () => {
+        //Set the angle to spin to based on current angle.
+        spinDegrees === 0 ? spinDegrees = 180 : spinDegrees = 0;
+    
+        menuButton.style.transform = `rotate(${spinDegrees}deg)`;
+    }
+})();
+
+//Image viewer logic.
+const Viewer = (() => {
+    const viewerBox = document.getElementById('viewerbox');
+    viewerBox.style.backgroundColor = 'pink';
+
+    let imageList = {};
+
+    //Add all mocchan images to the dom and then those elements to an array.
+    for (let i = 0; i < 6; i++) {
+        const newImg = document.createElement('img');
+        newImg.src = `images/mocchan${i + 1}.png`;
+        imageList[`moc${i + 1}`] = newImg; 
+    }
+
+    viewerBox.appendChild(imageList['moc1']);
+    viewerBox.appendChild(imageList['moc2']);
+
+    let lastScrollTop = 0;
+
+    viewerBox.addEventListener('scroll', () => {
+        
+        const st = viewerBox.pageXOffset || viewerBox.scrollLeft;
+        if (st > lastScrollTop){
+            console.log('scrolling down');
+        } else {
+           console.log('scrolling up');
+        }
+        lastScrollTop = st <= 0 ? 0 : st;
+    }, false);
+
+})();
